e3dc9bd4d427770aa4c85b5c245d57e1df7522fe
Brian Hicks <brian@brianthicks.com>
2023-07-07 10:17:02 -0500

    set up basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Opts {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Command {
+    /// Start a task
+    Start {
+        /// The name of the task you're working on
+        name: String,
+
+        /// How long you're planning to work, in minutes
+        #[arg(long, default_value = "25")]
+        duration: usize,
+    },
+
+    /// Take a break in between tasks
+    Break {
+        /// How long you're going to rest, in minutes
+        #[arg(long, default_value = "5")]
+        duration: usize,
+    },
+
+    /// Run background tasks, like being annoying when there's not an active task or break
+    /// running.
+    Daemon,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    println!("{:#?}", opts);
 }
