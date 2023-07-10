848550e4501cb16e2bf71f901201bdc18f68d44e
Brian Hicks <brian@brianthicks.com>
2023-07-10 17:24:49 -0500

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -155,6 +155,17 @@ enum Command {
 }
 
 fn main() -> Result<()> {
+    // a builder for `FmtSubscriber`.
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(tracing::Level::TRACE)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+
     color_eyre::install()?;
 
     Opts::parse().run()?;
