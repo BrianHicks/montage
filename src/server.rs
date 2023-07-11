c428b4a6f48e2d5ed3fca3d6cc73f914e54d6a53
Brian Hicks <brian@brianthicks.com>
2023-07-11 17:10:17 -0500

    we didn't need this; it's logged twice at INFO level

--- src/server.rs
+++ src/server.rs
@@ -15,8 +15,6 @@ type MontageSchema = Schema<Query, EmptyMutation, EmptySubscription>;
 
 #[tokio::main]
 pub async fn serve(addr: std::net::IpAddr, port: u16) {
-    tracing::info!("Listening on {addr}:{port}");
-
     let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
         .extension(async_graphql::extensions::Tracing)
         .finish();
