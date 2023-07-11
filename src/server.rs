909950b1547fb455055d2ad9e3eab3db1a23c80c
Brian Hicks <brian@brianthicks.com>
2023-07-11 16:46:04 -0500

    remove the hello route

--- src/server.rs
+++ src/server.rs
@@ -28,15 +28,13 @@ fn schema() -> Schema {
 pub async fn serve(addr: std::net::IpAddr, port: u16) {
     tracing::info!("Listening on {addr}:{port}");
 
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {name}!"));
-
     let state = warp::any().map(|| Context {});
     let graphql_filter = juniper_warp::make_graphql_filter(schema(), state.boxed());
 
     let graphiql = warp::path("graphiql").and(juniper_warp::graphiql_filter("/graphql", None));
     let graphql = warp::path("graphql").and(graphql_filter);
 
-    warp::serve(hello.or(graphiql).or(graphql))
+    warp::serve(graphiql.or(graphql))
         .run((addr, port))
         .await;
 }
