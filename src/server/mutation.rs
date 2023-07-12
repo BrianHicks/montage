ca018efb05368beee3b71b094914f903dcd5d02a
Brian Hicks <brian@brianthicks.com>
2023-07-11 23:16:08 -0500

    describe the start operation better

--- src/server/mutation.rs
+++ src/server/mutation.rs
@@ -1,20 +1,32 @@
+use super::session::Session;
+use async_graphql::context::Context;
 use async_graphql::Object;
 use color_eyre::eyre::Result;
-use async_graphql::context::Context;
-use super::session::Session;
 
 #[derive(Debug)]
 pub struct Mutation;
 
 #[Object]
 impl Mutation {
-    async fn start(&self, ctx: &Context<'_>) -> Result<Session> {
+    /// Start a new session
+    async fn start(
+        &self,
+        _context: &Context<'_>,
+        kind: String,
+        #[graphql(desc = "What will you be doing during this session?")] description: String,
+        #[graphql(desc = "How long will this session last?")] duration: chrono::Duration,
+        #[graphql(desc = "When did this session start? (Omit to start now)")] start_time: Option<
+            chrono::DateTime<chrono::Local>,
+        >,
+    ) -> Result<Session> {
+        let start = start_time.unwrap_or_else(|| chrono::Local::now());
+
         Ok(Session {
             id: 0,
-            kind: String::from("hey"),
-            description: String::from("desc"),
-            start_time: chrono::Local::now(),
-            end_time: chrono::Local::now(),
+            kind: kind,
+            description: description,
+            start_time: start,
+            end_time: start + duration,
         })
     }
 
