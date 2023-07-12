99f77b950545f5d022cf3b375114f6b77b8981b3
Brian Hicks <brian@brianthicks.com>
2023-07-12 16:41:55 -0500

    add a "not working" kind

--- src/server/kind.rs
+++ src/server/kind.rs
@@ -9,6 +9,9 @@ pub enum Kind {
 
     /// A shorter recovery session
     Break,
+
+    /// A longer recovery session like lunch, overnight, or the weekend
+    NotWorking,
 }
 
 impl Kind {
@@ -16,6 +19,7 @@ impl Kind {
         match self {
             Self::Task => Duration::minutes(25),
             Self::Break => Duration::minutes(5),
+            Self::NotWorking => Duration::minutes(60),
         }
     }
 }
