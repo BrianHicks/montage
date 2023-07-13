020ad5c974a71ec8e49a2c19a2c4bb7726882ea7
Brian Hicks <brian@brianthicks.com>
2023-07-13 13:44:24 -0500

    don't delete the file we need to build the project!

--- scripts/update-graphql-schema.sh
+++ scripts/update-graphql-schema.sh
@@ -5,7 +5,6 @@ cd $(git rev-parse --show-toplevel)
 
 CLIENT_FILE=src/client/schema.graphql
 
-if test -f "$CLIENT_FILE"; then rm "$CLIENT_FILE"; fi
+SCHEMA="$(cargo run show-graphql-schema)"
 
-printf '# NOTE: don''t change this by hand! Instead, run `%s` to get updates\n' "$0" > "$CLIENT_FILE"
-cargo run show-graphql-schema >> "$CLIENT_FILE"
+printf '# NOTE: don''t change this by hand! Instead, run `%s` to get updates\n%s' "$0" "$SCHEMA" > "$CLIENT_FILE"
