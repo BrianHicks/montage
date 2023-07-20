787a1e90087c24b228841b6e515526d8278b126c
Brian Hicks <brian@brianthicks.com>
2023-07-20 16:23:26 -0500

    add start and end time to report

--- src/server/report.rs
+++ src/server/report.rs
@@ -1,12 +1,18 @@
 use super::error::Result;
 use crate::server::Session;
 use async_graphql::SimpleObject;
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Duration, Local, Timelike};
 use sqlx::{Pool, Sqlite};
 
 /// A report on sessons started and ended during a given day plus some statistics.
 #[derive(SimpleObject, Debug)]
 pub struct Report {
+    /// The first date with sessions
+    pub start: DateTime<Local>,
+
+    /// The last date with sessions
+    pub end: DateTime<Local>,
+
     /// The sessions included in this report
     pub sessions: Vec<Session>,
 }
@@ -17,8 +23,28 @@ impl Report {
         start: DateTime<Local>,
         end: DateTime<Local>,
     ) -> Result<Self> {
-        let sessions = Session::for_range_inclusive(&pool, start, end).await?;
+        let start_date = Self::at_midnight(start);
+        let end_date = Self::at_midnight(end);
 
-        Ok(Self { sessions })
+        let sessions = Session::for_range_inclusive(&pool, start_date, end_date).await?;
+
+        Ok(Self {
+            sessions,
+            start: start_date,
+            end: end_date,
+        })
     }
+
+    fn at_midnight(date: DateTime<Local>) -> DateTime<Local> {
+        date.with_hour(0)
+            .unwrap()
+            .with_minute(0)
+            .unwrap()
+            .with_second(0)
+            .unwrap()
+            .with_nanosecond(0)
+            .unwrap()
+    }
+}
+
 }
