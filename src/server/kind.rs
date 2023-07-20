d831d75e453ce61cdcd80caf935c89f5289883bd
Brian Hicks <brian@brianthicks.com>
2023-07-20 16:15:51 -0500

    add a way to tell if a break is short or long

--- src/server/kind.rs
+++ src/server/kind.rs
@@ -1,4 +1,5 @@
 use chrono::Duration;
+use once_cell::sync::Lazy;
 
 /// What kind of session are we going to have?
 #[derive(async_graphql::Enum, Debug, PartialEq, Eq, Copy, Clone, sqlx::Type)]
@@ -17,5 +18,43 @@ impl Kind {
             Self::Task => Duration::minutes(25),
             Self::Break => Duration::minutes(5),
         }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum BreakKind {
+    Short,
+    Long,
+}
+
+static SHORT_VS_LONG_CUTOFF: Lazy<Duration> = Lazy::new(|| Duration::minutes(15));
+
+impl From<chrono::Duration> for BreakKind {
+    fn from(value: chrono::Duration) -> Self {
+        if value <= *SHORT_VS_LONG_CUTOFF {
+            Self::Short
+        } else {
+            Self::Long
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn break_kind_short() {
+        assert_eq!(BreakKind::from(Duration::zero()), BreakKind::Short)
+    }
+
+    #[test]
+    fn break_kind_at_threshold() {
+        assert_eq!(BreakKind::from(*SHORT_VS_LONG_CUTOFF), BreakKind::Short)
+    }
+
+    #[test]
+    fn break_kind_long() {
+        assert_eq!(BreakKind::from(Duration::days(1)), BreakKind::Long)
     }
 }
