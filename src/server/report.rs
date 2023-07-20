80c00bee30f282146c087f6cf5477dc0f044c388
Brian Hicks <brian@brianthicks.com>
2023-07-20 16:34:54 -0500

    add stub for totals

--- src/server/report.rs
+++ src/server/report.rs
@@ -1,11 +1,12 @@
 use super::error::Result;
 use crate::server::Session;
-use async_graphql::SimpleObject;
+use async_graphql::{ComplexObject, SimpleObject};
 use chrono::{DateTime, Duration, Local, Timelike};
 use sqlx::{Pool, Sqlite};
 
 /// A report on sessons started and ended during a given day plus some statistics.
 #[derive(SimpleObject, Debug)]
+#[graphql(complex)]
 pub struct Report {
     /// The first date with sessions
     pub start: DateTime<Local>,
@@ -15,6 +16,14 @@ pub struct Report {
 
     /// The sessions included in this report
     pub sessions: Vec<Session>,
+}
+
+#[ComplexObject]
+impl Report {
+    /// Aggregate totals of the time spent in sessions
+    async fn totals(&self) -> Totals {
+        self.get_totals()
+    }
 }
 
 impl Report {
@@ -45,6 +54,23 @@ impl Report {
             .with_nanosecond(0)
             .unwrap()
     }
+
+    pub fn get_totals(&self) -> Totals {
+        todo!()
+    }
 }
 
+/// Totals for each kind of session. If sessions started on one day and ended another, and the
+/// start or end date would cut part of that time off, we only count to or from midnight in the
+/// local time zone.
+#[derive(SimpleObject)]
+pub struct Totals {
+    /// The total time spent in short breaks (that is, those 15 minutes or less)
+    pub short_break: Duration,
+
+    /// The total time spent in long breaks (that is, those more than 15 minutes)
+    pub long_break: Duration,
+
+    /// The total time spent on tasks
+    pub task: Duration,
 }
