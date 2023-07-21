33c3e5bef7697bc72ae27f9529960987a1d1256e
Brian Hicks <brian@brianthicks.com>
2023-07-21 08:22:08 -0500

    add a query to get reports

--- montage_client/src/lib.rs
+++ montage_client/src/lib.rs
@@ -2,4 +2,5 @@ pub mod current_session;
 pub mod current_session_updates;
 pub mod extend_by;
 pub mod extend_to;
+pub mod report;
 pub mod start;
---
+++ montage_client/src/report.rs
@@ -0,0 +1,66 @@
+#[cynic::schema("montage")]
+mod schema {}
+
+#[derive(cynic::QueryVariables, Debug)]
+pub struct ReportQueryVariables {
+    pub start: DateTime,
+    pub end: DateTime,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(graphql_type = "Query", variables = "ReportQueryVariables")]
+pub struct ReportQuery {
+    #[arguments(end: $end, start: $start)]
+    pub report: Report,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct Report {
+    pub start: DateTime,
+    pub end: DateTime,
+    pub totals: Totals,
+    pub sessions: Vec<Session>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct Totals {
+    pub short_break: Duration,
+    pub long_break: Duration,
+    pub task: Duration,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct Session {
+    pub description: String,
+    pub actual_duration: Duration,
+    pub kind: Kind,
+    pub start_time: DateTime,
+}
+
+#[derive(cynic::Enum, Clone, Copy, Debug)]
+pub enum Kind {
+    Task,
+    Break,
+}
+
+type DateTime = chrono::DateTime<chrono::Local>;
+cynic::impl_scalar!(DateTime, schema::DateTime);
+
+type Duration = iso8601::Duration;
+cynic::impl_scalar!(Duration, schema::Duration);
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use cynic::QueryBuilder;
+
+    #[test]
+    fn gql_output() {
+        let operation = ReportQuery::build(ReportQueryVariables {
+            start: chrono::Local::now(),
+            end: chrono::Local::now(),
+        });
+
+        insta::assert_snapshot!(operation.query);
+    }
+}
