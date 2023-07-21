836d0bb41a9f0e32f66d57d3b26dae969d2a9cf9
Brian Hicks <brian@brianthicks.com>
2023-07-21 08:06:25 -0500

    clean up build files

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=migrations");
-
-    cynic_codegen::register_schema("montage")
-        .from_sdl_file("src/client/schema.graphql")
-        .unwrap()
-        .as_default()
-        .unwrap();
-}
