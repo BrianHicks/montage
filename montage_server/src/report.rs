e4cbbea709737ef27a47fff4db81e221aafe6939
Brian Hicks <brian@brianthicks.com>
2023-07-21 14:19:42 -0500

    add totals by description

--- montage_server/src/report.rs
+++ montage_server/src/report.rs
@@ -4,6 +4,7 @@ use super::{error::Result, kind::BreakKind};
 use async_graphql::{ComplexObject, SimpleObject};
 use chrono::{DateTime, Datelike, Duration, Local, Timelike};
 use sqlx::{Pool, Sqlite};
+use std::collections::HashMap;
 
 /// A report on sessons started and ended during a given day plus some statistics.
 #[derive(SimpleObject, Debug)]
@@ -83,6 +84,17 @@ pub struct Totals {
 
     /// The total time spent on tasks
     pub task: Duration,
+
+    /// Total time spent on tasks, broken down by task name
+    pub tasks_by_description: Vec<TotalByDescription>,
+}
+
+/// A description (of a task or break) and the total time spent on it during the report's time
+/// period.
+#[derive(SimpleObject, Debug, PartialEq, Eq)]
+pub struct TotalByDescription {
+    description: String,
+    total: Duration,
 }
 
 impl Default for Totals {
@@ -91,6 +103,7 @@ impl Default for Totals {
             short_break: Duration::zero(),
             long_break: Duration::zero(),
             task: Duration::zero(),
+            tasks_by_description: Vec::new(),
         }
     }
 }
@@ -102,6 +115,7 @@ impl Totals {
         end: DateTime<Local>,
     ) -> Self {
         let mut totals = Self::default();
+        let mut tasks_by_description = HashMap::with_capacity(sessions.len());
 
         let start_date = at_midnight(start);
         let end_date = at_one_second_to_midnight(end);
@@ -111,7 +125,13 @@ impl Totals {
             debug_assert!(session_total_within_dates >= Duration::zero());
 
             match session.kind {
-                Kind::Task => totals.task = totals.task + session_total_within_dates,
+                Kind::Task => {
+                    totals.task = totals.task + session_total_within_dates;
+                    tasks_by_description
+                        .entry(&session.description)
+                        .and_modify(|current| *current = *current + session_total_within_dates)
+                        .or_insert(session_total_within_dates);
+                }
                 Kind::Break => match BreakKind::from(session.get_actual_duration()) {
                     BreakKind::Short => {
                         totals.short_break = totals.short_break + session_total_within_dates
@@ -127,6 +147,14 @@ impl Totals {
         debug_assert!(totals.long_break >= Duration::zero());
         debug_assert!(totals.task >= Duration::zero());
 
+        totals.tasks_by_description = tasks_by_description
+            .drain()
+            .map(|(description, total)| TotalByDescription {
+                description: description.clone(),
+                total,
+            })
+            .collect();
+
         totals
     }
 }
@@ -138,7 +166,7 @@ mod test {
     fn session(kind: Kind, start: DateTime<Local>, duration: Duration, ended: bool) -> Session {
         Session {
             id: 0,
-            description: String::from(""),
+            description: String::from("description"),
             kind,
             start_time: start,
             duration,
@@ -165,6 +193,10 @@ mod test {
                 short_break: Duration::zero(),
                 long_break: Duration::zero(),
                 task: Duration::minutes(10),
+                tasks_by_description: vec![TotalByDescription {
+                    description: String::from("description"),
+                    total: Duration::minutes(10),
+                },],
             }
         )
     }
@@ -188,6 +220,7 @@ mod test {
                 short_break: Duration::minutes(10),
                 long_break: Duration::zero(),
                 task: Duration::zero(),
+                tasks_by_description: Vec::new(),
             }
         )
     }
@@ -211,6 +244,7 @@ mod test {
                 short_break: Duration::zero(),
                 long_break: Duration::hours(2),
                 task: Duration::zero(),
+                tasks_by_description: Vec::new(),
             }
         )
     }
@@ -236,6 +270,7 @@ mod test {
                 short_break: Duration::zero(),
                 long_break: Duration::hours(8),
                 task: Duration::zero(),
+                tasks_by_description: Vec::new(),
             }
         )
     }
