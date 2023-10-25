18a094ab0b156f75eaf797e4570a024f3e181296
Brian Hicks <brian@brianthicks.com>
2023-10-24 22:01:26 -0500

    add a button to extend a session

---
+++ Montage.omnifocusjs/Resources/extend.js
@@ -0,0 +1,43 @@
+"use strict";
+(() => {
+    var action = new PlugIn.Action(async () => {
+        try {
+            let focusForm = new Form();
+            focusForm.addField(new Form.Field.String("minutes", "Minutes", "5"));
+            await focusForm.show("Extend session", "Extend");
+            let values = focusForm.values;
+            console.log(values);
+            /// Step 2: start the session!
+            let req = URL.FetchRequest.fromString("http://localhost:4774/graphql");
+            if (!req?.url?.host) {
+                throw "could not parse the URL for the Montage API";
+            }
+            req.method = "POST";
+            req.bodyString = JSON.stringify({
+                query: "mutation ExtendByMutation($duration: String!) { extendBy(duration: $duration) { projectedEndTime } }",
+                variables: {
+                    duration: `PT${values.minutes}M`,
+                },
+            });
+            req.headers = { "Content-Type": "application/json" };
+            let resp = await req.fetch().catch((err) => {
+                console.error("Problem fetching tasks:", err);
+                let alert = new Alert("Problem extending session in Montage", err);
+                alert.show();
+                throw err;
+            });
+            if (resp.bodyString === null) {
+                throw "body string was null. Did the request succeed?";
+            }
+            console.log(`${resp.bodyString}`);
+            let data = JSON.parse(resp.bodyString).data.extendBy;
+            console.log(JSON.stringify(data));
+            let endTime = new Date(data.projectedEndTime);
+            new Alert("Extended session", `Extended sesion until ${endTime.getHours()}:${endTime.getMinutes()}`).show();
+        }
+        catch (err) {
+            console.error(err);
+        }
+    });
+    return action;
+})();
